40f6378e721e5204154df1113bf3ec74a7a5ee1a
neptunel <fsenaekiz19@gmail.com>
2023-04-13 09:29:03 -0700

    works on desktop

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -1,44 +1,58 @@
-/*
- * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
- * See LICENSE in the project root for license information.
- */
 
-/* global global, Office, self, window */
+// 1. How to construct online meeting details.
+// Not shown: How to get the meeting organizer's ID and other details from your service.
+const newBody = '<br>' +
+    '<a href="https://contoso.com/meeting?id=123456789" target="_blank">Join Contoso meeting</a>' +
+    '<br><br>' +
+    'Phone Dial-in: +1(123)456-7890' +
+    '<br><br>' +
+    'Meeting ID: 123 456 789' +
+    '<br><br>' +
+    'Want to test your video connection?' +
+    '<br><br>' +
+    '<a href="https://contoso.com/testmeeting" target="_blank">Join test meeting</a>' +
+    '<br><br>';
 
-Office.onReady(() => {
-  // If needed, Office.js is ready to be called
-});
+let mailboxItem;
 
-/**
- * Shows a notification when the add-in command is executed.
- * @param event {Office.AddinCommands.Event}
- */
-function action(event) {
-  const message = {
-    type: Office.MailboxEnums.ItemNotificationMessageType.InformationalMessage,
-    message: "Performed action.",
-    icon: "Icon.80x80",
-    persistent: true,
-  };
+// Office is ready.
+Office.onReady(function () {
+        mailboxItem = Office.context.mailbox.item;
+    }
+);
 
-  // Show a notification message
-  Office.context.mailbox.item.notificationMessages.replaceAsync("action", message);
-
-  // Be sure to indicate when the add-in command function is complete
-  event.completed();
+// 2. How to define and register a function command named `insertContosoMeeting` (referenced in the manifest)
+//    to update the meeting body with the online meeting details.
+function insertContosoMeeting(event) {
+    // Get HTML body from the client.
+    mailboxItem.body.getAsync("html",
+        { asyncContext: event },
+        function (getBodyResult) {
+            if (getBodyResult.status === Office.AsyncResultStatus.Succeeded) {
+                updateBody(getBodyResult.asyncContext, getBodyResult.value);
+            } else {
+                console.error("Failed to get HTML body.");
+                getBodyResult.asyncContext.completed({ allowEvent: false });
+            }
+        }
+    );
 }
+// Register the function.
+Office.actions.associate("insertContosoMeeting", insertContosoMeeting);
 
-function getGlobal() {
-  return typeof self !== "undefined"
-    ? self
-    : typeof window !== "undefined"
-    ? window
-    : typeof global !== "undefined"
-    ? global
-    : undefined;
+// 3. How to implement a supporting function `updateBody`
+//    that appends the online meeting details to the current body of the meeting.
+function updateBody(event, existingBody) {
+    // Append new body to the existing body.
+    mailboxItem.body.setAsync(existingBody + newBody,
+        { asyncContext: event, coercionType: "html" },
+        function (setBodyResult) {
+            if (setBodyResult.status === Office.AsyncResultStatus.Succeeded) {
+                setBodyResult.asyncContext.completed({ allowEvent: true });
+            } else {
+                console.error("Failed to set HTML body.");
+                setBodyResult.asyncContext.completed({ allowEvent: false });
+            }
+        }
+    );
 }
-
-const g = getGlobal();
-
-// The add-in command functions need to be available in global scope
-g.action = action;
